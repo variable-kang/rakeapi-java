aa0c43783f3a6dfb12a5a5a09429527f48ff2d87
강병수(Byeongsu Kang)/Data Infrastructure팀/SKP <byeongsu.kang@sk.com>
2016-08-10 11:38:44 +0900

    convert  testLog

--- core/src/test/java/com/rake/rakeapi/queue/FileQueueTest.java
+++ core/src/test/java/com/rake/rakeapi/queue/FileQueueTest.java
@@ -17,7 +17,7 @@ public class FileQueueTest {
     @Test
     public void FileQueueTest() throws Exception {
 
-        String testLog = "{\"service_id\":\"rakeapi-admin-test\",\"log\":\"강병수\":\"강병수\",\"channelCode\":\"tmap\",\"groupCode\":null,\"userKey\":null,\"invokeClassName\":\"com.skmnc.ndds.tmap.resource.notice.NoticeServiceResource\",\"invokeMethodName\":\"findPopupNotices\",\"serviceKrName\":\"요금제 정보 전달 조회\",\"resourceURI\":\"/etc/noticeservice/findpopupnotices\",\"httpMethod\":\"POST\",\"inputSize\":273,\"outputSize\":280389,\"startTime\":1378821711984,\"endTime\":1378821712026,\"success\":true,\"exceptionCode\":null,\"exceptionMessage\":null,\"terminalCode\":null,\"terminalMessage\":null,\"mdn\":\"01122223333\",\"sid\":\"61001610016000A\",\"fee\":\"61400\",\"sessionId\":\"100420130910230151959100\",\"trsSessionId\":\"\",\"server\":\"skmc-NDDTpCH4\",\"step\":\"D\",\"preService\":\"50\",\"menu\":\"2D\",\"reqMenu\":null,\"rpOption\":null,\"error\":null,\"verPhone\":\"HD4.1.2(459200)\",\"verEtc\":\"SHV-E210S\",\"poiIdMppNsl\":null,\"targetName\":null,\"tigMin\":null,\"svcType\":\"47\",\"verKit\":null,\"peekThread\":2,\"serverType\":\"S\",\"deviceId\":null,\"sendSid\":\"61001610016000A\",\"userId\":null,\"osType\":\"AND\",\"appType\":null,\"appExeType\":null,\"using\":\"MAIN\",\"macAddress\":\"\",\"tigMvnoInfo\":null,\"tigServiceNumber\":null,\"tauthWifi\":null";
+        String testLog = "{\"service_id\":\"rakeapi-admin-test\",\"log\":\"강병수\"}";
 
         Properties properties = new Properties();
         properties.setProperty("api.type", "dev");
--- core/src/test/java/com/rake/rakeapi/sender/KafkaTest.java
+++ core/src/test/java/com/rake/rakeapi/sender/KafkaTest.java
@@ -20,7 +20,7 @@ public class KafkaTest {
         properties.setProperty("sender.class", AsyncKafkaSender.class.getName());
         loggerAPI = LoggerAPI.getInstance(properties);
 
-        String testLog = "{\"ticketId\":\"100420130910230151959100\",\"channelCode\":\"tmap\",\"groupCode\":null,\"userKey\":null,\"invokeClassName\":\"com.skmnc.ndds.tmap.resource.notice.NoticeServiceResource\",\"invokeMethodName\":\"findPopupNotices\",\"serviceKrName\":\"요금제 정보 전달 조회\",\"resourceURI\":\"/etc/noticeservice/findpopupnotices\",\"httpMethod\":\"POST\",\"inputSize\":273,\"outputSize\":280389,\"startTime\":1378821711984,\"endTime\":1378821712026,\"success\":true,\"exceptionCode\":null,\"exceptionMessage\":null,\"terminalCode\":null,\"terminalMessage\":null,\"mdn\":\"01122223333\",\"sid\":\"61001610016000A\",\"fee\":\"61400\",\"sessionId\":\"100420130910230151959100\",\"trsSessionId\":\"\",\"server\":\"skmc-NDDTpCH4\",\"step\":\"D\",\"preService\":\"50\",\"menu\":\"2D\",\"reqMenu\":null,\"rpOption\":null,\"error\":null,\"verPhone\":\"HD4.1.2(459200)\",\"verEtc\":\"SHV-E210S\",\"poiIdMppNsl\":null,\"targetName\":null,\"tigMin\":null,\"svcType\":\"47\",\"verKit\":null,\"peekThread\":2,\"serverType\":\"S\",\"deviceId\":null,\"sendSid\":\"61001610016000A\",\"userId\":null,\"osType\":\"AND\",\"appType\":null,\"appExeType\":null,\"using\":\"MAIN\",\"macAddress\":\"\",\"tigMvnoInfo\":null,\"tigServiceNumber\":null,\"tauthWifi\":null}";
+        String testLog = "testLog";
         loggerAPI.log("rakeapi-admin-test", testLog);
         loggerAPI.close(true);
     }
@@ -34,7 +34,7 @@ public class KafkaTest {
         properties.setProperty("sender.class", AsyncKafkaSender.class.getName());
         loggerAPI = LoggerAPI.getInstance(properties);
 
-        String testLog = "{\"ticketId\":\"100420130910230151959100\",\"channelCode\":\"tmap\",\"groupCode\":null,\"userKey\":null,\"invokeClassName\":\"com.skmnc.ndds.tmap.resource.notice.NoticeServiceResource\",\"invokeMethodName\":\"findPopupNotices\",\"serviceKrName\":\"요금제 정보 전달 조회\",\"resourceURI\":\"/etc/noticeservice/findpopupnotices\",\"httpMethod\":\"POST\",\"inputSize\":273,\"outputSize\":280389,\"startTime\":1378821711984,\"endTime\":1378821712026,\"success\":true,\"exceptionCode\":null,\"exceptionMessage\":null,\"terminalCode\":null,\"terminalMessage\":null,\"mdn\":\"01122223333\",\"sid\":\"61001610016000A\",\"fee\":\"61400\",\"sessionId\":\"100420130910230151959100\",\"trsSessionId\":\"\",\"server\":\"skmc-NDDTpCH4\",\"step\":\"D\",\"preService\":\"50\",\"menu\":\"2D\",\"reqMenu\":null,\"rpOption\":null,\"error\":null,\"verPhone\":\"HD4.1.2(459200)\",\"verEtc\":\"SHV-E210S\",\"poiIdMppNsl\":null,\"targetName\":null,\"tigMin\":null,\"svcType\":\"47\",\"verKit\":null,\"peekThread\":2,\"serverType\":\"S\",\"deviceId\":null,\"sendSid\":\"61001610016000A\",\"userId\":null,\"osType\":\"AND\",\"appType\":null,\"appExeType\":null,\"using\":\"MAIN\",\"macAddress\":\"\",\"tigMvnoInfo\":null,\"tigServiceNumber\":null,\"tauthWifi\":null}";
+        String testLog = "testLog";
         loggerAPI.log("rakeapi-admin-test", testLog);
         loggerAPI.close(true);
     }
